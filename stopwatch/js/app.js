eff8a352c7311fdc71e27f5f8e771a2f00bab8ce
vasishak <vasishak.d@gmail.com>
2022-01-23 20:24:53 +0500

    stopwatch save values

--- stopwatch/js/app.js
+++ stopwatch/js/app.js
@@ -4,14 +4,26 @@ let rest = false;
 let sec = 0;
 let timer;
 let timerElement = document.getElementById('timer');
-let currentCycleElement = document.getElementById('current-cycle');
-let currentCycle = 1;
+let currentSetElement = document.getElementById('current-set');
+let currentRoundElement = document.getElementById('current-round');
+let currentSet = 1;
+let currentRound = 1;
 let bigRestSeconds;
-let totalCycles;
+let totalSets;
 let isBigRest;
 let minutes;
 let seconds;
 let isStart = false;
+const localStorage = window.localStorage;
+const inputsWrapper = document.getElementById('inputs');
+const inputs = inputsWrapper.querySelectorAll('input');
+const workElement = document.getElementById('sec-to-work');
+const setsElement = document.getElementById('sets');
+const restElement = document.getElementById('sec-to-rest');
+const bigRestElement = document.getElementById('big-rest');
+const startElement = document.getElementById('start');
+
+const SPACE_KEY = 32;
 
 function callTimer() {
     sec++;
@@ -20,10 +32,10 @@ function callTimer() {
          if (sec === secToWork && !rest) {
              sec = 0;
 
-            if (currentCycle === totalCycles) {
+            if (currentSet === totalSets) {
                 isBigRest = true;
                 timerElement.style.color = '#33d470';
-                currentCycleElement.innerHTML = 'REST';
+                currentSetElement.innerHTML = 'REST';
             } else {
                 rest = true;
                 timerElement.style.color = '#d64141';
@@ -32,27 +44,24 @@ function callTimer() {
         } else if (sec === secToRest && rest) {
             sec = 0;
             rest = false;
-            currentCycle++;
+            currentSet++;
             timerElement.style.color = '#000';
-            currentCycleElement.innerHTML = currentCycle;
+            currentSetElement.innerHTML = currentSet;
         }
-    } else {
-        if (sec === bigRestSeconds) {
-            isBigRest = false;
-            currentCycle = 1;
-            sec = 0;
-            minutes = 0;
-            seconds = 0;
-            timerElement.style.color = '#000';
-            currentCycleElement.innerHTML = currentCycle;
-        }
+    } else if (sec === bigRestSeconds) {
+        isBigRest = false;
+        currentSet = 1;
+        sec = 0;
+        minutes = 0;
+        seconds = 0;
+        currentRoundElement.innerHTML = currentRound;
+        timerElement.style.color = '#000';
+        currentSetElement.innerHTML = currentSet;
     }
 
-    if (sec > 60) {
-        minutes = Math.floor(sec / 60);
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        seconds = sec - minutes * 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
+    if (sec > 59) {
+        minutes = Math.floor(sec / 60).toString().padStart(2, '0');
+        seconds = (sec - minutes * 60).toString().padStart(2, '0');
         timerElement.innerHTML = minutes + ':' + seconds;
     } else {
         timerElement.innerHTML = sec;
@@ -61,24 +70,32 @@ function callTimer() {
 
 
 function start() {
+    if (isStart) {
+        return;
+    }
+
+    startElement.disabled = true;
+    isStart = true;
+    currentSetElement.innerHTML = currentSet;
+    currentRoundElement.innerHTML = currentRound;
+    totalSets = parseInt(setsElement.value);
     sleep.prevent();
     setTimeout(function() {
-        document.getElementById('start').disabled = true;
-        isStart = true;
-        secToWork = parseInt(document.getElementById('sec-to-work').value) + 1;
-        secToRest = parseInt(document.getElementById('sec-to-rest').value) + 1;
-        bigRestSeconds = parseInt(document.getElementById('big-rest').value) + 1;
-        totalCycles = parseInt(document.getElementById('cycles').value);
-        currentCycleElement.innerHTML = currentCycle;
+        secToWork = parseInt(workElement.value) + 1;
+        secToRest = parseInt(restElement.value) + 1;
+        bigRestSeconds = parseInt(bigRestElement.value) + 1;
         timer = setInterval(callTimer, 1000);
     }, 1000);
 }
 
 function stop() {
-    sleep.allow();
-    isStart = false;
-    document.getElementById('start').disabled = false;
+    if (!isStart) {
+        return;
+    }
     clearInterval(timer);
+    isStart = false;
+    startElement.disabled = false;
+    sleep.allow();
 }
 
 function reset() {
@@ -86,16 +103,23 @@ function reset() {
     sec = 0;
     rest = false;
     isBigRest = false;
-    currentCycle = 1;
+    currentSet = 1;
+    currentRound = 1;
     minutes = 0;
     seconds = 0;
-    currentCycleElement.innerHTML = 0;
+    currentSetElement.innerHTML = 0;
+    currentRoundElement.innerHTML = 0;
     timerElement.style.color = '#000';
     timerElement.innerHTML = sec;
 }
 
+function toDefault() {
+    localStorage.clear();
+    inputs.forEach(input => input.value = input.getAttribute('default-value'));
+}
+
 document.onkeyup=function(e){
-  if(e.which === 32) {
+  if(e.which === SPACE_KEY) {
     if (isStart) {
         stop()
     } else {
@@ -103,3 +127,14 @@ document.onkeyup=function(e){
     }
   }
 }
+
+inputs.forEach(input => {
+    input.value = localStorage.getItem(input.id) || input.value;
+});
+
+
+inputsWrapper.addEventListener('change', function(event){
+    element = event.target;
+
+    localStorage.setItem(element.id, element.value);
+});
